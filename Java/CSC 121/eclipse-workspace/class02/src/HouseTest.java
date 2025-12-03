26c002ad3c8f856dbe88bbf6c87b5c31ccb9a6c6
cj-whaley <cj.whaley@vikings.berry.edu>
2025-12-02 18:24:22 -0600

    Getting repo linked to VSC
    Also fixed test case errors on class02, changed headers and added public to test methods

--- Java/CSC 121/eclipse-workspace/class02/src/HouseTest.java
+++ Java/CSC 121/eclipse-workspace/class02/src/HouseTest.java
@@ -1,22 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 //examples and tests of House objects and methods
-class HouseTest {
+public class HouseTest {
 
 	  House ranch = new House("Ranch", 2500, "25 Maple St", 375000);
 	  House colon = new House("Colonial", 2000, "18 Thelberg Rd", 250000);
 	  House cape = new House("Cape", 4000, "12 Timberline Dr", 400000);
 	  
 	  @Test
-	  void testCostPerSqFt() {
+	  public void testCostPerSqFt() {
 	    assertEquals(150, this.ranch.costPerSqFt());
 	    assertEquals(125, this.colon.costPerSqFt());
 	    assertEquals(100, this.cape.costPerSqFt());
 	  }
 	  
 	  @Test
-	  void testMoreExpensiveThan() {
+	  public void testMoreExpensiveThan() {
 	    assertEquals(false, this.ranch.moreExpensiveThan(400000));
 	    assertEquals(true, this.ranch.moreExpensiveThan(100000));
 	    assertEquals(false, this.cape.moreExpensiveThan(400000));
@@ -24,13 +24,13 @@ class HouseTest {
 	  }
 	  
 	  @Test
-	  void testMorePerSqFtThan() {
+	  public void testMorePerSqFtThan() {
 	    assertEquals(true, this.colon.morePerSqFtThan(cape));
 	    assertEquals(false, this.colon.morePerSqFtThan(ranch));
 	  }
 	  
 	  @Test
-	  void testComputeTax() {
+	  public void testComputeTax() {
 	    assertEquals(7500, this.ranch.computeTax());
 	    assertEquals(3750, this.colon.computeTax());
 	    assertEquals(8000, this.cape.computeTax());
