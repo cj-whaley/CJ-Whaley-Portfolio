e12f5bfd5994f574ee361ee486e0db5f9aa7eaf7
cj-whaley <cj.whaley@vikings.berry.edu>
2025-12-02 21:16:46 -0600

    fixed test errors in hw03

--- Java/CSC 121/eclipse-workspace/hw03-containment/src/BankAccountTest.java
+++ Java/CSC 121/eclipse-workspace/hw03-containment/src/BankAccountTest.java
@@ -1,8 +1,7 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
-import org.junit.jupiter.api.Test;
-
-class BankAccountTest {
+public class BankAccountTest {
 
 	BankCustomer cp = new BankCustomer("Cole", "Porter");
 	BankCustomer ak = new BankCustomer("Alex", "Knapp");
@@ -15,7 +14,7 @@ class BankAccountTest {
 	BankAccount boSavings = new BankAccount(bo, 0);
 	
 	@Test
-	void testStatusMessage() {
+	public void testStatusMessage() {
 		assertEquals("Positive Balance", this.cpChecking.statusMessage());
 		assertEquals("Positive Balance", this.cpSavings.statusMessage());
 		assertEquals("No Money", this.boSavings.statusMessage());
--- Java/CSC 121/eclipse-workspace/hw03-containment/src/BankCustomerTest.java
+++ Java/CSC 121/eclipse-workspace/hw03-containment/src/BankCustomerTest.java
@@ -1,15 +1,14 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
-import org.junit.jupiter.api.Test;
-
-class BankCustomerTest {
+public class BankCustomerTest {
 
 	BankCustomer cp = new BankCustomer("Cole", "Porter");
 	BankCustomer ak = new BankCustomer("Alex", "Knapp");
 	BankCustomer bo = new BankCustomer("Brenden", "O'Sullivan");
 	
 	@Test
-	void testOpenAccount() {
+	public void testOpenAccount() {
 		assertEquals(new BankAccount(cp, 2000), this.cp.openAccount(2000));
 		assertEquals(new BankAccount(ak, 1), this.ak.openAccount(1));
 		assertEquals(new BankAccount(bo, 6900), this.bo.openAccount(6900));
